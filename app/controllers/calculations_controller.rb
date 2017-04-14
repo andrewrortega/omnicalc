098e49f66bf6802014b8e37fccf5977bdcdf2950
Andrew Ortega <andrewrortega@chicagobooth.edu>
2017-04-13 20:57:51 -0500

    Update 3
    Updated some of the stats functions

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -94,27 +94,27 @@ class CalculationsController < ApplicationController
     # The numbers the user input are in the array @numbers.
     # ================================================================================
 
-    @sorted_numbers = "Replace this string with your answer."
+    @sorted_numbers = @numbers.sort
 
-    @count = "Replace this string with your answer."
+    @count = @numbers.count
 
-    @minimum = "Replace this string with your answer."
+    @minimum = @numbers.min
 
-    @maximum = "Replace this string with your answer."
+    @maximum = @numbers.max
 
-    @range = "Replace this string with your answer."
-
-    @median = "Replace this string with your answer."
-
-    @sum = "Replace this string with your answer."
-
-    @mean = "Replace this string with your answer."
-
-    @variance = "Replace this string with your answer."
-
-    @standard_deviation = "Replace this string with your answer."
-
-    @mode = "Replace this string with your answer."
+    @range = @maximum - @minimum
+    #
+    @median = @numbers.median
+    #
+    # @sum = @numbers.sum
+    #
+    # @mean = @sum / @count
+    #
+    # @variance = @numbers.var
+    #
+    # @standard_deviation = @numbers.standard_deviation
+    #
+    # @mode = @numbers.mode
 
     # ================================================================================
     # Your code goes above.
